1bf6c3191d37e8c1f6dd165771b2e5a5a365981e
samuelti <samuelti2442@gmail.com>
2021-08-26 15:07:50 -0700

    trying to fix the project 1 image

--- src/components/PortfolioContainer/portfolioContainer.js
+++ src/components/PortfolioContainer/portfolioContainer.js
@@ -4,7 +4,7 @@ import Project2 from '../Project2/project2'
 
 export default function PortfolioContainer() {
   return (
-<section>
+<section style={{display: "flex", flexDirection: "row"}}>
     <Project1 />
     <Project2 />
 </section>
