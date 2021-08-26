1603fc109b23fc0b97bc691b7bb5206b1dc10bc8
samuelti <samuelti2442@gmail.com>
2021-08-26 15:02:53 -0700

    trying to fix the project 1 image

--- src/components/Project1/project1.js
+++ src/components/Project1/project1.js
@@ -3,13 +3,13 @@ import "./project1.css";
 
 export default function Project1() {
   return (
-    <section
-    className="project1"
+    <div
+    className="work project1"
     >
       <div >
         <a href="https://github.com/ts795/mini-project-third-party.git">Cocktail Library</a>
         <a href="https://hugh18019.github.io/Drinks-Library/"><i className="fab fa-github"></i></a>
       </div>
-    </section>
+    </div>
   );
 }
--- src/components/Project2/project2.js
+++ src/components/Project2/project2.js
@@ -3,13 +3,13 @@ import "./project2.css";
 
 export default function Project2() {
   return (
-    <section
-    className="project2"
+    <div
+    className=" work project2"
     >
       <div >
         <a href="https://remembering-calculator.herokuapp.com/">Remember Calculator</a>
         <a href="https://github.com/samuelti/Remeber_Calculator"><i className="fab fa-github"></i></a>
       </div>
-    </section>
+    </div>
   );
 }
