9ece509aea922baf466720a0f5a746e6f3b08d91
samuelti <samuelti2442@gmail.com>
2021-09-10 13:51:52 -0700

    pushing new css and react changes

--- src/components/PortfolioContainer/portfolioContainer.js
+++ src/components/PortfolioContainer/portfolioContainer.js
@@ -1,12 +1,66 @@
-import React from 'react';
-import Project1 from '../Project1/project1';
-import Project2 from '../Project2/project2'
+import React from "react";
+
+import "./portfolioContainer.css";
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
 
 export default function PortfolioContainer() {
+  const projectList = [
+    {
+      name: "Cocktail Database",
+      linkGithub: "https://github.com/ts795/mini-project-third-party.git",
+      linkDeployed: "https://hugh18019.github.io/Drinks-Library/",
+      img: "https://samuelti.github.io/My-Class-Repo/02_homework/Assets/Capture.JPG",
+    },
+    {
+      name: "Remember Calculator",
+      linkGithub: "https://github.com/samuelti/Remeber_Calculator",
+      linkDeployed: "https://remembering-calculator.herokuapp.com/",
+      img: "https://samuelti.github.io/My-Class-Repo/02_homework/Assets/Calculator.JPG",
+    },
+    {
+      name: "README Generator",
+      linkGithub: "https://github.com/samuelti/08-README-Generator/blob/master/Develop/index.js",
+      linkDeployed: "",
+      img: "",
+    },
+    {
+      name: "Password Generator",
+      linkGithub: "https://github.com/samuelti/3-passwordGenerator.git",
+      linkDeployed: "https://samuelti.github.io/3-passwordGenerator/index.html",
+      img: "",
+    },
+    {
+      name: "Note Taker",
+      linkGithub: "https://github.com/samuelti/11_Note_Taker",
+      linkDeployed: "https://cool-note-taker-for-cool-dudes.herokuapp.com/notes",
+      img: "",
+    },
+    {
+      name: "Day Planner",
+      linkGithub: "https://github.com/samuelti/EventPlanner",
+      linkDeployed: "https://samuelti.github.io/EventPlanner/",
+      img: "",
+    },
+  ];
   return (
-<section style={{display: "flex", flexDirection: "row"}}>
-    <Project1 />
-    <Project2 />
-</section>
+    <Container>
+    
+      {projectList.map((project) => {
+        return (
+          <div className="work">
+            <div style={{backgroundImage: `url(${project.img})`}} className="image">
+              <a href={project.linkDeployed}>
+                {project.name}
+              </a>
+              <a href={project.linkGithub}>
+                <i className="fab fa-github"></i>
+              </a>
+            </div>
+          </div>
+        );
+      })}
+    
+    </Container>
   );
 }
